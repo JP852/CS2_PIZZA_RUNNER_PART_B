ae262015bfb8d16c1368ff43416d6513e61e7a7d
JP852 <142391590+JP852@users.noreply.github.com>
2023-10-25 16:56:14 +0100

    Update Question 3.sql

--- Question 3.sql
+++ Question 3.sql
@@ -10,3 +10,21 @@ WHERE RO.pickup_time != 'null'
 ,CO.order_time
 ,RO.pickup_time
 ORDER BY MINS DESC
+
+or with the CTE to calcualte the AVG for the number of pizzas
+
+WITH CTE AS(
+    SELECT 
+co.order_id
+,COUNT(pizza_id) AS number_of_pizzas
+,MAX(datediff('minute',pickup_time::timestamp_ntz,order_time)*-1) AS PREP_TIME
+FROM runner_orders AS ro
+INNER JOIN customer_orders AS co
+ON ro.order_id = co.order_id
+WHERE ro.pickup_time != 'null'
+GROUP BY co.order_id
+)
+SELECT AVG(PREP_TIME)
+,number_of_pizzas
+FROM CTE
+GROUP BY number_of_pizzas
