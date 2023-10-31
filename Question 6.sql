1fc37ba9c67c593d831494ea6938aeedfc5920f2
JP852 <142391590+JP852@users.noreply.github.com>
2023-10-31 17:43:30 +0000

    Update Question 6.sql

--- Question 6.sql
+++ Question 6.sql
@@ -1,1 +1,11 @@
-Q
+SELECT 
+  runner_id, 
+  order_id, 
+  REPLACE(distance, 'km', '')::numeric(3, 1) / REGEXP_REPLACE(duration, '[^0-9]', '')::numeric(3, 1) as speed_km_per_minute 
+FROM 
+  runner_orders 
+WHERE 
+  duration <> 'null' 
+ORDER BY 
+  runner_id, 
+  order_id 
