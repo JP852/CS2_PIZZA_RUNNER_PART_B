97bf088229a2908628b5865faf309633fe3b6f54
John Power <142391590+JP852@users.noreply.github.com>
2024-04-30 15:35:35 +0100

    Delete Question 2.sql

--- Question 2.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-ro.runner_id
-,AVG(datediff('minute',pickup_time::timestamp_ntz,co.order_time))*-1 AS AVG_MINS
-FROM customer_orders AS CO 
-INNER JOIN
-runner_orders as RO
-ON co.order_id = ro.order_id
-WHERE pickup_time != 'null'
-GROUP BY ro.runner_id
